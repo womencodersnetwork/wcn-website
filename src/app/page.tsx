8602779ba484126cae62a275d45e6411a68f5bee
alarrabee <alexlarrabee10@gmail.com>
2024-04-30 21:13:21 -0500

    wip landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,17 +5,20 @@ import Link from 'next/link';
 const Home = () => {
   return (
     <div>
-      <Navbar />
+      {/* <Navbar /> */}
       <main className="flex flex-col items-center justify-center min-h-screen p-4">
         <h1 className="text-4xl font-bold text-center">
-          Welcome to Women Everywhere in Tech Website!
+          Welcome to the Women Everywhere in Tech Website!
         </h1>
+        <div>
+          <p className="text-2xl text-center">While we're hard at work crafting an amazing website to serve you better, we're thrilled to invite you to join our Discord community! Connect with fellow women in tech, share ideas, seek support, and learn together.</p>
+        </div>
         <div className="mt-8">
-          <Link href="/signup">
-              Join Now
-          </Link>
-          <Link href="/discord">
-              Discord
+          {/* <Link href="/signup">
+          <div className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Join Now</div>
+          </Link> */}
+          <Link href="https://discord.gg/YpaJ3JckNM">
+            <div className="bg-indigo-500 hover:bg-indigo-600 text-white font-bold py-2 px-4 rounded">Join Our Discord!</div>
           </Link>
         </div>
       </main>
