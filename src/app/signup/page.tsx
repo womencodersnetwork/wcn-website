e61c2672e83591f0fe67947f9aeb177283febb31
Asako Hayase <chipmunk.kck@gmail.com>
2024-05-07 23:06:32 -0700

    Update ESLint configuration, refactor classNames

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,11 +1,11 @@
-import React from 'react';
-import Navbar from '../components/Navbar';
+import React from "react";
+import Navbar from "../components/Navbar";
 
 const Signup = () => {
   return (
     <div>
       <Navbar />
-      <main className="flex flex-col items-center justify-center min-h-screen p-4 text-center">
+      <main className="flex min-h-screen flex-col items-center justify-center p-4 text-center">
         <h1 className="text-4xl font-bold">Member Sign Up</h1>
         {/* More content goes here */}
       </main>
